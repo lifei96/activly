a41103860beca7753c6698364f7eb3eebe27452a
Fei Li <lifei9696@gmail.com>
2018-11-14 17:17:32 -0500

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:card_settings/card_settings.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(MyApp());
 
