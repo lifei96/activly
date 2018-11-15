8e51c6fb7f7b9881de5f61909de1f4bcc3e163e6
Fei Li <lifei9696@gmail.com>
2018-11-14 22:05:09 -0500

    persistent Google sign in

--- lib/main.dart
+++ lib/main.dart
@@ -8,26 +8,6 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
 
-  final GoogleSignIn _googleSignIn = GoogleSignIn();
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-
-  Future<FirebaseUser> _handleSignIn() async {
-    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-    FirebaseUser user = await _auth.signInWithGoogle(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-    print("signed in " + user.displayName);
-    return user;
-  }
-
-  Future<void> _handleSignOut() async {
-    await _googleSignIn.signOut();
-    await _auth.signOut();
-    print("signed out");
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -51,33 +31,105 @@ class MyApp extends StatelessWidget {
         indicatorColor: Colors.white,
         scaffoldBackgroundColor: Colors.grey[100],
       ),
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.calendar_today)),
-                Tab(icon: Icon(Icons.directions_run)),
-                Tab(icon: Icon(Icons.settings)),
-              ],
-            ),
-            title: Text('Activ.ly'),
-          ),
-          body: TabBarView(
-            children: [
-              createCalendarTabBarView(),
-              Icon(Icons.directions_run),
-              createSettingsTabBarView(),
+      home: HomePage()
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    scopes: [
+      'profile',
+      'email',
+      'openid',
+      'https://www.googleapis.com/auth/calendar.events',
+    ],
+  );
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  FirebaseUser _currentUser;
+
+  Future<FirebaseUser> _trySignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signInSilently();
+    if (googleUser == null) {
+      return null;
+    }
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    FirebaseUser user = await _auth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    print("silently signed in " + user.displayName);
+    return user;
+  }
+
+  Future<FirebaseUser> _handleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    FirebaseUser user = await _auth.signInWithGoogle(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+    print("signed in " + user.displayName);
+    return user;
+  }
+
+  Future<void> _handleSignOut() async {
+    await _googleSignIn.signOut();
+    await _auth.signOut();
+    print("signed out");
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance
+    // as done by the _increment method above.
+    // The Flutter framework has been optimized to make rerunning
+    // build methods fast, so that you can just rebuild anything that
+    // needs updating rather than having to individually change
+    // instances of widgets.
+    if (_currentUser == null) {
+      _trySignIn().then((user) {
+        if (user != null) {
+          setState(() {
+            _currentUser = user;
+          });
+        }
+      });
+    }
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.calendar_today)),
+              Tab(icon: Icon(Icons.directions_run)),
+              Tab(icon: Icon(Icons.settings)),
             ],
           ),
+          title: Text('Activ.ly'),
+        ),
+        body: TabBarView(
+          children: [
+            createCalendarTabBarView(),
+            Icon(Icons.directions_run),
+            createSettingsTabBarView(),
+          ],
         ),
       ),
     );
   }
 
   Form createCalendarTabBarView() {
-    var signedIn = _googleSignIn.currentUser == null? false : true;
+    var signedIn = _currentUser == null? false : true;
     return Form(
       key: Key('calendar_form'),
       child: CardSettings(
@@ -86,25 +138,42 @@ class MyApp extends StatelessWidget {
             label: 'Calendar',
           ),
           CardSettingsButton(
+            label: 'Current account: ' + (signedIn ? _currentUser.email : ''),
+            backgroundColor: Colors.white,
+            bottomSpacing: 0.0,
+            onPressed: null,
+            visible: signedIn,
+          ),
+          CardSettingsButton(
             label: 'Sign in with Google',
             backgroundColor: Colors.white,
             textColor: Colors.orange[700],
             bottomSpacing: 4.0,
             onPressed: () {
               _handleSignIn()
-                .then((FirebaseUser user) => print(user))
+                .then((FirebaseUser user) {
+                  print(user);
+                  setState(() {
+                    _currentUser = user;
+                  });
+                })
                 .catchError((e) => print(e));
             },
             visible: !signedIn,
           ),
           CardSettingsButton(
             label: 'Sign out',
-            backgroundColor: Colors.white,
-            textColor: Colors.red,
+            backgroundColor: Colors.red[400],
+            textColor: Colors.white,
             bottomSpacing: 4.0,
             onPressed: () {
               _handleSignOut()
-                .then((result) => print('signed out'))
+                .then((result) {
+                  print('signed out');
+                  setState(() {
+                    _currentUser = null;
+                  });
+                })
                 .catchError((e) => print(e));
             },
             visible: signedIn,
@@ -173,9 +242,4 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
-
-  void importGoogleCalender() {
-
-  }
-
 }
